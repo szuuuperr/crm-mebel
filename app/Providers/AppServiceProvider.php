24a3772b429915b03392a45001099cb1a817fdcb
szuuuperr <adyatma.yafi19@gmail.com>
2026-09-13 16:24:40 +0700

    Fix Trusted Proxy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\Notification;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -22,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+
         View::composer('*', function ($view) {
             if (Auth::check()) {
                 $unreadNotifications = Notification::where('user_id', Auth::id())
